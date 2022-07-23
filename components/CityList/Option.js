1e9b991a104e6b291c8e304dc412b8c1d4d4b5b7
James <j606888@gmail.com>
2022-07-23 11:11:46 +0800

    ➕ Add styled-components

--- components/CityList/Option.js
+++ components/CityList/Option.js
@@ -2,30 +2,21 @@ import { Box } from '@mui/material'
 import React from 'react'
 
 const Option = ({ children, isBlack=false }) => {
-  if (isBlack) {
-    return (
-      <Box
-        sx={{
-          backgroundColor: "#757575",
-          color: "#FFFFFF",
-          borderRadius: 8,
-          paddingY: 1,
-          paddingX: 2,
-        }}
-      >
-        {children}
-      </Box>
-    )  
+  const defaultStyle = {
+    borderRadius: 8,
+    paddingY: 1,
+    paddingX: 2,
+    backgroundColor: "#EDEDED",
+    color: "#757575",
   }
+  if (isBlack) {
+    defaultStyle['backgroundColor'] = "#757575"
+    defaultStyle['color'] = '#FFFFFF'
+  }
+
   return (
     <Box
-      sx={{
-        backgroundColor: "#EDEDED",
-        color: "#757575",
-        borderRadius: 8,
-        paddingY: 1,
-        paddingX: 2
-      }}
+      sx={defaultStyle}
     >
       {children}
     </Box>
