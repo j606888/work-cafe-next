ed7af325187bd1b95e31df9acfc5f75f1504aab1
James <j606888@gmail.com>
2022-07-23 20:58:55 +0800

    Add Filter to weekday

---
+++ src/components/Filter.stories.js
@@ -0,0 +1,9 @@
+import Filter from "./Filter";
+
+export default {
+  title: "components/filter",
+  component: Filter
+}
+
+export const Template = (args) => <Filter {...args} />
+Template.args = {}
