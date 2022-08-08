8b10ac15b61146689beef22bfc100626da759ea7
James <j606888@gmail.com>
2022-08-08 23:48:35 +0800

    Introduce react-select

---
+++ src/components/Select/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { default as ReactSelect } from "react-select"
+
+const colourStyles = {
+  control: (styles, {}) => ({ ...styles, backgroundColor: "white"}),
+  option: (styles, {}) => {
+    return {
+      ...styles,
+    }
+  },
+  multiValue: (styles, { data }) => ({
+    ...styles,
+    color: "#999999",
+    backgroundColor: "#E1F5FE",
+    border: "1px solid #e9eefa",
+    margin: "5px 5px 5px 0",
+  }),
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    backgroundColor: "#E1F5FE",
+  })
+}
+
+const Select = ({ options }) => {
+  const labelOptions = options.map(option => {
+    return {value: option, label: option}
+  })
+
+  const handleChange = (e) => {
+    console.log(e)
+  }
+  return (
+    <ReactSelect
+      options={labelOptions}
+      isMulti
+      placeholder="Choose City..."
+      styles={colourStyles}
+      defaultValue={[labelOptions[0], labelOptions[1]]}
+      onChange={handleChange}
+    />
+  )
+}
+export default Select
---
+++ stories/Select.stories.js
@@ -0,0 +1,12 @@
+import Select from "@/components/Select"
+import cityMap from "@/config/cityMap"
+
+export default {
+  title: "components/Select",
+}
+
+const cities = cityMap.map(city => city.name)
+export const Default = (args) => <Select {...args} />
+Default.args = {
+  options: cities
+}
