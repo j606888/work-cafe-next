c3927d91acebe5b1d46b9b84d7746650e5a7fd6b
James <j606888@gmail.com>
2022-08-09 23:52:53 +0800

    RatingSelect with storybook

---
+++ stories/RatingSelect.stories.js
@@ -0,0 +1,8 @@
+import RatingSelect from "@/components/Select/RatingSelect"
+
+export default {
+  title: "components/RatingSelect",
+}
+
+export const Default = (args) => <RatingSelect {...args} />
+Default.args = {}
