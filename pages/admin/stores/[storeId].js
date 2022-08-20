a197a13568daaa5952a77fe07917f99521013bee
James <j606888@gmail.com>
2022-08-20 16:34:02 +0800

    Add button to sync-photos

--- pages/admin/stores/[storeId].js
+++ pages/admin/stores/[storeId].js
@@ -4,10 +4,14 @@ import { useRouter } from "next/router"
 import AdminLayout from "components/AdminLayout"
 import StoreDetail from "components/StoreDetail"
 import StoreOpeningHours from "components/StoreOpeningHours"
-import { getStore } from "api/stores"
-import { ImageListItem, ImageList } from "@mui/material"
+import { getStore, syncStorePhotos } from "api/stores"
+import { ImageListItem, ImageList, Button } from "@mui/material"
 
 const Container = styled.div``
+
+const Control = styled.div`
+  margin-bottom: 1rem;
+`
 
 const StandardImageList = ({ photos }) => {
   if (!photos) return null
@@ -26,21 +30,38 @@ const Store = () => {
   const router = useRouter()
   const { storeId } = router.query
   const [store, setStore] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function callAPI() {
+    const res = await getStore(storeId)
+    setStore(res)
+  }
 
   useEffect(() => {
-    async function callAPI() {
-      const res = await getStore(storeId)
-      setStore(res)
-    }
-
     if (storeId) callAPI()
   }, [storeId])
+
+  async function handleSyncPhoto() {
+    setIsLoading(true)
+    await syncStorePhotos(store.placeId)
+    callAPI()
+    setIsLoading(false)
+  }
 
   return (
     <AdminLayout>
       <Container>
         {store && (
           <>
+            <Control>
+              <Button
+                variant="contained"
+                onClick={handleSyncPhoto}
+                disabled={isLoading}
+              >
+                Sync Photo
+              </Button>
+            </Control>
             <StoreDetail {...store} />
             <br />
             <StoreOpeningHours
