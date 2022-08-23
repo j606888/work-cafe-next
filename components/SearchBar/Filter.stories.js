82cdebf763d303b1b41a7470ec3e466b81c0f35a
James <j606888@gmail.com>
2022-08-23 23:41:56 +0800

    Remove outdated component

--- components/SearchBar/Filter.stories.js
+++
@@ -1,9 +0,0 @@
-import Filter from "./Filter";
-
-export default {
-  title: "Searchbar/Filter",
-  component: Filter,
-}
-
-export const Template = (args) => <Filter {...args} />
-Template.args = {}
--- components/SearchBar/index.stories.js
+++
@@ -1,9 +0,0 @@
-import SearchBar from "./index"
-
-export default {
-  title: "Searchbar/index",
-  component: SearchBar,
-}
-
-export const Template = (args) => <SearchBar {...args} />
-Template.args = {}
