4ca2bfa76b3b53a2c10ab5abb2fba7649b099661
James <j606888@gmail.com>
2022-08-27 18:24:44 +0800

    Add features/Header

---
+++ features/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from './styled'
+
+const Header = () => {
+  return (
+    <Container>
+      <h3>Work Cafe | Taiwan</h3>
+      <div>
+        <a href="#">註冊</a>
+        <a href="#">登入</a>
+      </div>
+    </Container>
+  )
+}
+
+export default Header
---
+++ features/Header/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #eee;
+  height: 64px;
+  padding: 0 1rem;
+  font-family: 'Noto Sans', sans-serif;
+
+  h3, a {
+    color: #757575;
+  }
+
+  a {
+    text-decoration: none;
+    margin: 0 0.6rem;
+  }
+`
