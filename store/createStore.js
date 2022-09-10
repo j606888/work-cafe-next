575610e671b1df7532975ce3adac55de8d02a91d
James <j606888@gmail.com>
2022-09-11 00:48:10 +0800

    Add StoreSlice

--- store/createStore.js
+++ store/createStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import searchReducer from './slices/search'
+import storeReducer from './slices/store'
 
 export default configureStore({
   reducer: {
     search: searchReducer,
+    store: storeReducer,
   }
 })
--- store/slices/store.js
+++ store/slices/store.js
@@ -3,6 +3,19 @@ import { createSlice } from '@reduxjs/toolkit'
 export const storeSlice = createSlice({
   name: 'store',
   initialState: {
-
+    mode: 'MAP',
+    stores: []
+  },
+  reducers: {
+    changeMode: (state, action) => {
+      state.mode = action.payload
+    },
+    updateStores: (state, action) => {
+      state.stores = action.payload
+    }
   }
 })
+
+export const { changeMode, updateStores } = storeSlice.actions
+
+export default storeSlice.reducer
