edc311c3e2fda6acbb6e734059e7457e07ebcc21
James <j606888@gmail.com>
2022-09-11 16:51:28 +0800

    ReviewForm html css done

--- features/ReviewForm/index.js
+++ features/ReviewForm/index.js
@@ -1,6 +1,15 @@
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material"
 import React from "react"
 import RecommendBlock from "./RecommendBlock"
-import { Temp, Form } from './styled'
+import { Temp, Form, Scroll, Buttons } from "./styled"
 
 const ReviewForm = ({ name, onChange = () => {} }) => {
   const handleRecommendChange = (recommend) => {
@@ -11,7 +20,76 @@ const ReviewForm = ({ name, onChange = () => {} }) => {
     <Temp>
       <Form>
         <h3>{name}</h3>
-        <RecommendBlock onChange={handleRecommendChange}/>
+        <Scroll>
+          <RecommendBlock onChange={handleRecommendChange} />
+          <TextField
+            name="description"
+            multiline
+            fullWidth
+            rows={4}
+            placeholder="說明你在這間店的體驗"
+          />
+          <FormControl>
+            <FormLabel>音量</FormLabel>
+            <RadioGroup name="roomVolume">
+              <FormControlLabel
+                value="quite"
+                control={<Radio />}
+                label="幾乎無人說話"
+              />
+              <FormControlLabel
+                value="normal"
+                control={<Radio />}
+                label="輕聲交談"
+              />
+              <FormControlLabel
+                value="loud"
+                control={<Radio />}
+                label="正常交談"
+              />
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel>限時規定</FormLabel>
+            <RadioGroup name="timeLimit">
+              <FormControlLabel value="no" control={<Radio />} label="無限時" />
+              <FormControlLabel
+                value="weekend"
+                control={<Radio />}
+                label="週末限時"
+              />
+              <FormControlLabel
+                value="yes"
+                control={<Radio />}
+                label="有限時"
+              />
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel>插座供應</FormLabel>
+            <RadioGroup name="socketSupport">
+              <FormControlLabel
+                value="yes"
+                control={<Radio />}
+                label="大部分有插座"
+              />
+              <FormControlLabel
+                value="normal"
+                control={<Radio />}
+                label="些許插座"
+              />
+              <FormControlLabel
+                value="no"
+                control={<Radio />}
+                label="沒有插座"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Scroll>
+        <Buttons>
+          <Button variant="outlined">取消</Button>
+          <Button variant="contained">送出</Button>
+        </Buttons>
       </Form>
     </Temp>
   )
--- features/ReviewForm/styled.js
+++ features/ReviewForm/styled.js
@@ -4,18 +4,34 @@ export const Temp = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #ccc;
   height: 100vh;
+  box-sizing: border-box;
 `
 
 export const Form = styled.form`
   background-color: #fff;
-  padding: 1rem;
-  width: 480px;
+  width: 360px;
   border-radius: 12px;
+  box-shadow: 0 0 4px 1px rgba(0,0,0,0.2);  
 
   h3 {
     text-align: center;
     margin: 1rem;
   }
+`
+
+export const Scroll = styled.div`
+  height: 60vh;
+  overflow-y: scroll;
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const Buttons = styled.div`
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
 `
