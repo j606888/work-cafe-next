9ce17b5d7970468e7d08741a17a97e8caeec8fbf
James <j606888@gmail.com>
2022-09-16 20:53:32 +0800

    Tweak code

--- features/SearchStoreList/index.js
+++ features/SearchStoreList/index.js
@@ -1,3 +1,4 @@
+import { fetcher } from "api"
 import SearchHere from "components/Button/SearchHere"
 import OpenTimeV2 from "features/OpenTimeV2"
 import SearchbarV2 from "features/SearchbarV2"
@@ -8,9 +9,11 @@ import {
   SearchbarV2Container,
   SearchHereContainer,
   StoreListContainer,
-} from "features/UserStoreMap/styled"
+} from "features/UserMap/styled"
 import React, { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { updatePlaceId, updateStores } from "store/slices/store"
+import useSWR from "swr"
 
 const calcSearchHereLeft = (stores, store) => {
   const leftMap = {
@@ -30,23 +33,28 @@ const INIT_OPTIONS = {
   openHour: null,
   go: false,
 }
-const SearchStoreList = ({ store, onSearch = () => {}, onClearPlaceId = () => {} }) => {
+const SearchStoreList = ({ store, mapCenter }) => {
+  const dispatch = useDispatch()
   const [options, setOptions] = useState(INIT_OPTIONS)
   const [openDrawer, setOpenDrawer] = React.useState(false)
-  const { stores } = useSelector((state) => state.store)
+  const [center, setCenter] = useState({
+    lat: 23.0042325,
+    lng: 120.2216038,
+  })
+  const { data: stores } = useSWR(
+    options?.go ? ["/stores/location", { ...options, ...center, limit: 20 }] : null,
+    fetcher
+  )
 
   const handleCloseDrawer = () => {
     setOpenDrawer(false)
   }
-
   const handleKeywordSearch = (keyword) => {
     setOptions((cur) => ({ ...cur, keyword, go: true }))
   }
-
   const handleClear = () => {
     setOptions((cur) => ({ ...cur, keyword: "", go: false }))
   }
-
   const handleOpenTimeChange = ({ openType, openWeek, openHour }) => {
     const currentOpenTime = {
       openType,
@@ -55,15 +63,19 @@ const SearchStoreList = ({ store, onSearch = () => {}, onClearPlaceId = () => {}
     }
     setOptions((cur) => ({ ...cur, ...currentOpenTime, go: true }))
   }
-
   const handleSearch = () => {
     setOptions((cur) => ({ ...cur,  go: true }))
+    setCenter(mapCenter)
   }
 
   useEffect(() => {
-    onSearch(options)
-    onClearPlaceId()
+    dispatch(updatePlaceId(null))
   }, [options, openDrawer])
+
+  useEffect(() => {
+    dispatch(updateStores(stores || []))
+  }, [stores, dispatch])
+
 
   return (
     <>
--- features/UserMap/index.js
+++ features/UserMap/index.js
@@ -9,7 +9,7 @@ import StoreDetail from "features/StoreDetail"
 import { StoreDetailContainer, MyLocationContainer } from "./styled"
 import BookmarkListV2 from "features/BookmarkListV2"
 import { useSelector, useDispatch } from "react-redux"
-import { updateStores, updatePlaceId } from "store/slices/store"
+import { updatePlaceId } from "store/slices/store"
 import HiddenListV2 from "features/HiddenListV2"
 import ReviewList from "features/ReviewList"
 import useInitMap from "hooks/useInitMap"
@@ -17,43 +17,29 @@ import MyLocation from "features/MyLocation"
 import MeMarker from "features/MyLocation/MeMarker"
 import SearchStoreList from "features/SearchStoreList"
 
-const UserStoreMap = () => {
+const UserMap = () => {
   const dispatch = useDispatch()
   const { isReady, mapSettings, map, setMap } = useInitMap()
   const [showCardHead, setShowCardHead] = React.useState(false)
   const { stores, mode, placeId, bouncePlaceId } = useSelector(
     (state) => state.store
   )
-  const mapZoom = useRef(15)
-  const meCenter = useRef(null)
-  const [options, setOptions] = React.useState({})
-  const mapCenterRef = useRef({
-    lat: 23.0042325,
-    lng: 120.2216038,
-  })
-  const { data: locationStores, mutate: mutateLocation } = useSWR(
-    options?.go ? ["/stores/location", { ...options, limit: 20 }] : null,
+  const myLocation = useRef(null)
+  const { data: store, mutate: mutateStore } = useSWR(
+    placeId ? `/stores/${placeId}` : null,
     fetcher
   )
-  const { data: store, mutate: mutateStore } = useSWR(placeId ? `/stores/${placeId}` : null, fetcher)
 
-  const handleSearch = (searchOptions) => {
-    setOptions({ ...searchOptions, ...mapCenterRef.current })
-    mutateLocation()
-  }
   const handleOnIdle = ({ lat, lng, zoom }) => {
     const mapPath = `@${lat},${lng},${zoom}z`
     Router.push({
       pathname: `/${mapPath}`,
     })
     localStorage.setItem("lastLocation", mapPath)
-    mapCenterRef.current = { lat, lng }
-    mapZoom.current = zoom
   }
   const handleRefreshStore = (placeId) => {
     dispatch(updatePlaceId(placeId))
     mutateStore()
-    mutateLocation()
   }
   const clearPlaceId = () => {
     dispatch(updatePlaceId(null))
@@ -66,13 +52,8 @@ const UserStoreMap = () => {
     const center = { lat, lng }
     map.setZoom(15)
     map.panTo(center)
-    mapCenterRef.current = center
-    meCenter.current = center
+    myLocation.current = center
   }
-
-  useEffect(() => {
-    dispatch(updateStores(locationStores))
-  }, [locationStores])
 
   useEffect(() => {
     if (store && map) {
@@ -80,24 +61,31 @@ const UserStoreMap = () => {
         lat: store.lat,
         lng: store.lng,
       }
-      if (mapZoom.current < 15) {
+      if (map.zoom < 15) {
         map.setZoom(15)
       }
       map.panTo(center)
       map.panBy(-400, 0)
-
-      mapCenterRef.current = center
     }
   }, [store])
 
   if (!isReady) return <div>NotReady</div>
 
-  const me = meCenter.current && (
-    <MeMarker lat={meCenter.current.lat} lng={meCenter.current.lng} />
+  const me = myLocation.current && (
+    <MeMarker lat={myLocation.current.lat} lng={myLocation.current.lng} />
   )
 
   return (
     <>
+      <MyLocationContainer>
+        <MyLocation onClick={handleFindMe} />
+      </MyLocationContainer>
+      {mode === "MAP" && (
+        <SearchStoreList store={store} mapCenter={map?.center?.toJSON()} />
+      )}
+      {mode === "BOOKMARK" && <BookmarkListV2 />}
+      {mode === "HIDDEN" && <HiddenListV2 />}
+      {mode === "REVIEW" && <ReviewList />}
       {store && (
         <StoreDetailContainer onScroll={handleScroll}>
           <StoreDetail
@@ -108,9 +96,6 @@ const UserStoreMap = () => {
           />
         </StoreDetailContainer>
       )}
-      <MyLocationContainer>
-        <MyLocation onClick={handleFindMe} />
-      </MyLocationContainer>
       <GoogleMapWrapper
         map={map}
         setMap={setMap}
@@ -127,19 +112,9 @@ const UserStoreMap = () => {
             onClick={handleRefreshStore}
           />
         ))}
-        {mode === "MAP" && (
-          <SearchStoreList
-            store={store}
-            onSearch={handleSearch}
-            onClearPlaceId={clearPlaceId}
-          />
-        )}
-        {mode === "BOOKMARK" && <BookmarkListV2 />}
-        {mode === "HIDDEN" && <HiddenListV2 />}
-        {mode === "REVIEW" && <ReviewList />}
       </GoogleMapWrapper>
     </>
   )
 }
 
-export default UserStoreMap
+export default UserMap
