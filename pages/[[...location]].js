3bd78c84148669e2e7a7a105020a11e788a0f43c
James <j606888@gmail.com>
2022-09-18 15:39:17 +0800

    Add head title to index

--- pages/[[...location]].js
+++ pages/[[...location]].js
@@ -1,9 +1,13 @@
 import AccountMenu from "features/AccountMenu";
 import GoogleMap from "features/UserMap";
+import Head from "next/head";
 
 export default function MapPage() {
   return (
     <>
+      <Head>
+        <title>Work Cafe</title>
+      </Head>
       <AccountMenu />
       <GoogleMap />
     </>
