dec25e9cf6469da250beffab8a9e148fad02561e
James <j606888@gmail.com>
2022-09-18 16:04:55 +0800

    Add Admin dashboard

--- features/AdminDashboard/Card/index.js
+++ features/AdminDashboard/Card/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Avatar } from '@mui/material'
 import { Container } from './styled'
 
-const Card = ({ title, number, icon, bgcolor }) => {
+const Card = ({ title, value, icon, bgcolor }) => {
   const Icon = icon
   return (
     <Container>
       <div className="detail">
         <span>{title}</span>
-        <h3>{number}</h3>
+        <h3>{value}</h3>
       </div>
       <Avatar sx={{ bgcolor: bgcolor, width: 48, height: 48 }}>
         <Icon />
--- features/AdminDashboard/index.js
+++ features/AdminDashboard/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 import {
   InsertPhoto as InsertPhotoIcon,
   Comment as CommentIcon,
   Store as StoreIcon,
   Person as PersonIcon,
-} from '@mui/icons-material'
-import Card from './Card'
+} from "@mui/icons-material"
+import Card from "./Card"
+import useSWR from "swr"
+import { fetcher } from "api"
+import { Skeleton } from "@mui/material"
 
 const Container = styled.div`
   padding: 1.5rem;
@@ -18,39 +21,65 @@ const Container = styled.div`
   }
 `
 
-const mockData = [
+const REPORTS = [
   {
-    title: "Total Store",
-    number: "3,600",
+    title: "店家數量",
+    key: "storeCount",
     icon: StoreIcon,
     bgcolor: "#D14343",
   },
   {
-    title: "Total User",
-    number: "230",
+    title: "總用戶",
+    key: "userCount",
     icon: PersonIcon,
     bgcolor: "#14B8A6",
   },
   {
-    title: "Total Photo",
-    number: "1,350",
+    title: "照片量",
+    key: "storePhotoCount",
     icon: InsertPhotoIcon,
     bgcolor: "#FEAF1F",
   },
   {
-    title: "Total Comments",
-    number: "2,400",
+    title: "評論數",
+    key: "reviewCount",
     icon: CommentIcon,
     bgcolor: "#5048E4",
   },
 ]
 
+const SkeletonCard = () => (
+  <Skeleton variant="rectangular" width={"100%"} height={180} />
+)
+
 const Dashboard = () => {
+  const { data } = useSWR(`/admin/report/dashboard`, fetcher)
+  const loading = !Boolean(data)
+
+  if (loading) {
+    return (
+      <Container>
+        <div className="cards">
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+          <SkeletonCard />
+        </div>
+      </Container>
+    )
+  }
+
   return (
     <Container>
       <div className="cards">
-        {mockData.map((data) => (
-          <Card key={data.title} {...data} />
+        {REPORTS.map(({ title, key, icon, bgcolor }) => (
+          <Card
+            key={key}
+            title={title}
+            icon={icon}
+            bgcolor={bgcolor}
+            value={data[key]}
+          />
         ))}
       </div>
     </Container>
