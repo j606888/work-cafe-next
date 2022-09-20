3847724a48ab7ff9eeb296649a749d4dfb7c5a1f
James <j606888@gmail.com>
2022-09-20 20:03:48 +0800

    Remove search store

--- store/createStore.js
+++ store/createStore.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import searchReducer from './slices/search'
 import storeReducer from './slices/store'
 
 export default configureStore({
   reducer: {
-    search: searchReducer,
     store: storeReducer,
   }
 })
