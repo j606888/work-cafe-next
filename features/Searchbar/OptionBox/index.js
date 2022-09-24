707e613b1621857b643504514feeaca00c66b13f
James <j606888@gmail.com>
2022-09-24 18:34:32 +0800

    Tweak search box

--- features/Searchbar/OptionBox/index.js
+++ features/Searchbar/OptionBox/index.js
@@ -2,9 +2,9 @@ import StoreOption from "../StoreOption"
 import LocationOption from "../LocationOption"
 import { Box } from "@mui/system"
 
-const OptionBox = ({ props, option, inputValue }) => {
+const OptionBox = ({ props, option, inputValue, onClick }) => {
   return (
-    <Box {...props} >
+    <Box {...props} onClick={onClick}>
       {option.type === "store" ? (
         <StoreOption {...option} inputValue={inputValue} />
       ) : (
