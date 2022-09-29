c7aa954a71c013354637c1c0467cae8326e2fc5b
James <j606888@gmail.com>
2022-09-29 22:05:23 +0800

    Clean up old searchBar

--- pages/backup-index.js
+++ pages/backup-index.js
@@ -1,13 +1,11 @@
 import About from "features/About";
 import CityQuickSearch from "features/CityQuickSearch";
 import Header from "features/Header";
-import Hero from "features/Hero";
 
 export default function RootPage() {
   return (
     <>
       <Header />
-      <Hero />
       <About />
       <CityQuickSearch />
     </>
