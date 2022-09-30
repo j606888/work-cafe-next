a327d6f43ff260521d2ff52b68e53d3efe641d4f
James <j606888@gmail.com>
2022-09-30 21:08:24 +0800

    Server start

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import { fetcher } from "api"
 import { ThemeProvider } from "styled-components"
 import styledTheme from "constant/styled-theme"
 
+if (typeof window === 'undefined') {
+  const { server } = require('../mocks/server')
+  server.listen()
+} else {
+  const { worker } = require('../mocks/browser')
+  worker.start()
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <SWRConfig
