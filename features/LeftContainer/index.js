b6fb1f71cc813b104af5b214d93cba6f98b1e4c4
James <j606888@gmail.com>
2022-10-11 22:50:35 +0800

    Move component to WelcomeBlock

--- features/LeftContainer/index.js
+++ features/LeftContainer/index.js
@@ -1,9 +1,6 @@
-import { CircularProgress, Divider } from "@mui/material"
-import Searchbar from "features/Searchbar"
 import SearchFilter from "features/SearchFilter"
 import StoreDetail from "features/StoreDetail"
 import StoreList from "features/StoreList"
-import WelcomeMessage from "features/WelcomeMessage"
 import useStoreStore from "stores/useStoreStore"
 import { useEffect } from "react"
 import styled from "styled-components"
@@ -14,10 +11,9 @@ import useControlMap from "hooks/useControlMap"
 import useLocationParamsStore from "stores/useLocationParamsStore"
 import shallow from "zustand/shallow"
 import useInitMap from "hooks/useInitMap"
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
 import useFindMe from "hooks/useFindMe"
-import WhereToGo from "./WhereToGo"
-import { devices } from 'constant/styled-theme'
+import { devices } from "constant/styled-theme"
+import WelcomeBlock from "./WelcomeBlock/WelcomeBlock"
 
 const Container = styled.div`
   width: 628px;
@@ -27,89 +23,6 @@ const Container = styled.div`
   @media ${devices.iphoneSE} {
     width: 100%;
     z-index: 5;
-  }
-`
-
-const Or = styled.p`
-  text-align: center;
-  color: #757575;
-  font-size: 14px;
-  padding: 0.5rem 0 1rem;
-
-  @media ${devices.iphoneSE} {
-    font-size: 12px;
-
-    padding: 0;
-  }
-`
-
-const Upper = styled.div`
-  background-color: #f9f9f9;
-  padding-bottom: 1rem;
-  position: relative;
-`
-
-const SearchContainer = styled.div`
-  display: flex;
-  padding: 1rem 56px 0;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  flex-wrap: wrap;
-
-  @media ${devices.iphoneSE} {
-  padding: 1rem 23px 0;
-  }
-`
-
-const HelloContainer = styled.div`
-  padding: 56px 0 0 56px;
-  color: #757575;
-
-  p {
-    font-size: 14px;
-    margin: 4px 0 8px;
-  }
-
-  @media ${devices.iphoneSE} {
-    padding: 0;
-    text-align: center;
-
-    p {
-      font-size: 12px;
-      margin: 4px auto;
-    }
-
-  }
-`
-
-const SearchNearbyButton = styled.div`
-  color: #757575;
-  background-color: #f5f5f5;
-  border: 1px solid #757575;
-  border-radius: 8px;
-  font-size: 16px;
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translate(-50%, 50%);
-  padding: 12px 25px 12px 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  cursor: pointer;
-  z-index: 3;
-
-  &:hover {
-    background-color: #fcfcfc;
-  }
-
-  @media ${devices.iphoneSE} {
-    padding: 6px 12px 6px 6px;
-    font-size: 14px;
-    white-space: nowrap;
-    gap: 0.5rem;
   }
 `
 
@@ -171,24 +84,11 @@ const LeftContainer = () => {
 
   return (
     <Container>
-      <Upper>
-        {!data && (
-          <HelloContainer>
-            <WhereToGo />
-            <p>Work Cafe 幫你快速篩選 適合辦公的咖啡店</p>
-          </HelloContainer>
-        )}
-        <SearchContainer>
-          <Searchbar onSearch={handleSearch} />
-          {/* <SearchFilter onChange={handleFilterChange} /> */}
-        </SearchContainer>
-        <Or>或</Or>
-        <SearchNearbyButton onClick={handleNearbySearch}>
-          {loading ? <CircularProgress size={24} /> : <ArrowForwardIcon />}
-          <span>搜尋 我附近的咖啡店</span>
-        </SearchNearbyButton>
-      </Upper>
-      {!data && !placeId && <WelcomeMessage />}
+      <WelcomeBlock
+        loading={loading}
+        onSearch={handleSearch}
+        onNearbySearch={handleNearbySearch}
+      />
       {data && data.length === 0 && <NoMatch />}
       {!placeId && <StoreList stores={data || []} onClick={handleClickStore} />}
       {placeId && (
