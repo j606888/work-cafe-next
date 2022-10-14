80930fe933fe92308bdff634ea532e2960ecb460
James <j606888@gmail.com>
2022-10-15 00:13:05 +0800

    Tweak CSS

--- features/StoreDetailV2/Reviews/WorkCafeReviews.js
+++ features/StoreDetailV2/Reviews/WorkCafeReviews.js
@@ -8,7 +8,7 @@ const Container = styled.div`
   display: flex;
   gap: 1rem;
   align-items: flex-start;
-  margin-bottom: 2rem;
+  /* margin-bottom: 2rem; */
 `
 
 const Content = styled.div`
@@ -63,43 +63,49 @@ const ImageBox = styled.div`
   height: 120px;
   width: 120px;
   border-radius: 12px;
-  background-color: #EDEDED;
+  background-color: #ededed;
   position: relative;
 
-  ${({ more }) => more && css`
-    background-color: #bbb;
-    &::after {
-      content: '+1';
-      font-size: 24px;
-      color: #fff;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%, -50%);
-    }
-  `}
+  ${({ more }) =>
+    more &&
+    css`
+      background-color: #bbb;
+      &::after {
+        content: "+1";
+        font-size: 24px;
+        color: #fff;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+    `}
 
   @media ${devices.iphoneSE} {
     flex: none;
   }
 `
 
-const WorkCafeReviews = ({ userName, userAvatarUrl, createdAt, description, }) => {
+const WorkCafeReviews = ({
+  userName,
+  userAvatarUrl,
+  createdAt,
+  description,
+}) => {
   return (
-    <Container>
-      <Avatar alt={userName} src={userAvatarUrl}/>
-      <Content>
-        <h6>{userName}</h6>
-        <span>{_dateString(createdAt)}</span>
-        <p>
-          {description}
-        </p>
-        <ImagesWorm images={[1,1,1,1,1,1]}/>
-      </Content>
-    </Container>
+    <>
+      <Container>
+        <Avatar alt={userName} src={userAvatarUrl} />
+        <Content>
+          <h6>{userName}</h6>
+          <span>{_dateString(createdAt)}</span>
+          <p>{description}</p>
+        </Content>
+      </Container>
+      <ImagesWorm images={[1, 1, 1, 1, 1, 1]} />
+    </>
   )
 }
-
 
 function _dateString(timestamp) {
   const time = new Date(timestamp * 1000)
@@ -107,7 +113,7 @@ function _dateString(timestamp) {
   const month = time.getMonth()
   const date = time.getDate()
   const day = time.getDay()
-  const isWeekend = [5,6].includes(day) ? "週末造訪" : "平日造訪"
+  const isWeekend = [5, 6].includes(day) ? "週末造訪" : "平日造訪"
 
   return `${year}年${month}月${date}日・${isWeekend}`
 }
