89055aca0b6dc0a18ba2aa47b2a6c6233bfda681
James <j606888@gmail.com>
2022-10-14 19:45:50 +0800

    Fix my location

--- stores/useMapStoreV2.js
+++ stores/useMapStoreV2.js
@@ -3,8 +3,10 @@ import create from "zustand"
 const useMapStoreV2 = create((set, get) => ({
   map: null,
   center: null,
+  myLocation: null,
   setMap: (map) => set({ map }),
-  setCenter: (latLng) => set({ center: latLng })
+  setCenter: (latLng) => set({ center: latLng }),
+  setMyLocation: (latLng) => set({ myLocation: latLng })
 }))
 
 export default useMapStoreV2
