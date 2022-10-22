f5977125e631878e95d1f6b1c31ee8a59efdf90d
James <j606888@gmail.com>
2022-10-22 16:07:56 +0800

    Tweak code

--- pages/stores/[placeId].js
+++ pages/stores/[placeId].js
@@ -7,15 +7,14 @@ const PlaceRedirect = () => {
   const { placeId } = router.query
   const { data, error } = useSWR(placeId ? `/stores/${placeId}` : null)
 
-  if (!placeId) return <h2>No placeId found</h2>
+  if (!router.isReady) return <h2>Loading...</h2>
   if (error) return <h2>Place Not Found</h2>
   if (data) {
     const { lat, lng } =data
     const newPath = `/@${lat},${lng},15z/${placeId}`
     router.push(newPath)
   }
-
-  return <div>loading...</div>
+  return <div>Loading...</div>
 }
 
 export default PlaceRedirect
