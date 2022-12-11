57b757e7683ef6869ae2a921ae97137051e9bdf8
James <j606888@gmail.com>
2022-12-11 18:44:58 +0800

    Search one store

--- stores/useStoreSWR.js
+++ stores/useStoreSWR.js
@@ -1,14 +1,21 @@
 import useSWR from "swr"
 import useLocationParamsStore from "./useLocationParamsStore"
 import { isEmpty } from "lodash"
+import useStoreStore from "./useStoreStore"
 
 const useStoreSWR = () => {
   const params = useLocationParamsStore((state) => state.params)
   const queryString = new URLSearchParams(params).toString()
+  const setPlaceId = useStoreStore((state) => state.setPlaceId)
   const shouldFetch = !isEmpty(params)
   const path = `stores/location?${queryString}`
 
   const { data, isLoading } = useSWR(shouldFetch ? path : null)
+
+  if (data && data.stores.length === 1) {
+    const placeId = data.stores[0].placeId
+    setPlaceId(placeId)
+  }
 
   return {
     data,
