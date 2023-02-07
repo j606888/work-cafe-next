8feb38f2c0d9b047733f4fd5f2c514fe399684fa
James <j606888@gmail.com>
2023-02-08 00:11:30 +0800

    SearchFilter desktop done

--- features/v2/SearchFilter/index.js
+++ features/v2/SearchFilter/index.js
@@ -1,41 +1,59 @@
 import { Dialog, Drawer, useMediaQuery } from "@mui/material"
 import { OPEN_HOURS, OPEN_WEEKS } from "constants/openTime"
 import { devices } from "constants/styled-theme"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import ButtonWithCount from "./ButtonWithCount"
 import FilterForm from "./FilterForm"
 
 const SearchFilter = () => {
+  const storedFilters = JSON.parse(localStorage.getItem("filters"))
+  const router = useRouter()
   const [open, setOpen] = useState(false)
-  const [filterCount, setFilterCount] = useState(0)
+  const [filterCount, setFilterCount] = useState(+localStorage.getItem("filterCount"))
   const fullScreen = useMediaQuery(devices.mobileXl)
-  const [openTime, setOpenTime] = useState("NONE")
-  const [openWeek, setOpenWeek] = useState(OPEN_WEEKS[0].value)
-  const [openHour, setOpenHour] = useState(OPEN_HOURS[0].value)
-  const [wakeUp, setWakeUp] = useState(false)
-  const [tagIds, setTagIds] = useState([])
+
+  const [openTime, setOpenTime] = useState(storedFilters?.openTime || "NONE")
+  const [openWeek, setOpenWeek] = useState(storedFilters?.openWeek || OPEN_WEEKS[0].value)
+  const [openHour, setOpenHour] = useState(storedFilters?.openHour || OPEN_HOURS[0].value)
+  const [wakeUp, setWakeUp] = useState(storedFilters?.wakeUp || false)
+  const [tagIds, setTagIds] = useState(storedFilters?.tagIds || [])
 
   function handleClose() {
     setOpen(false)
   }
 
   function handleApply({ openTime, openWeek, openHour, wakeUp, tagIds }) {
+    let count = 0
+    if (openTime !== "NONE") count+=1
+    if (wakeUp) count+=1
+    count += tagIds.length
+    setFilterCount(count)
+
     if (openTime) setOpenTime(openTime)
     if (openWeek) setOpenWeek(openWeek)
     if (openHour) setOpenHour(openHour)
     setWakeUp(wakeUp)
     setTagIds(tagIds)
 
+    let query = {
+      openTime,
+      openWeek,
+      openHour,
+      wakeUp,
+      tagIds,
+    }
+    query = _filterCleanSettings(query)
+    localStorage.setItem("filters", JSON.stringify(query))
+    const pathWithoutQuery = router.asPath.split("?")[0]
+    router.push({ pathname: pathWithoutQuery, query })
+
     setOpen(false)
   }
 
   useEffect(() => {
-    let count = 0
-    if (openTime !== "NONE") count+=1
-    if (wakeUp) count+=1
-    count += tagIds.length
-    setFilterCount(count)
-  }, [openTime, wakeUp, tagIds])
+    localStorage.setItem("filterCount", filterCount)
+  }, [filterCount])
 
   return (
     <>
@@ -63,6 +81,23 @@ const SearchFilter = () => {
       )}
     </>
   )
+}
+
+function _filterCleanSettings(settings) {
+  const result = {}
+
+  if (settings.openTime !== 'NONE') result.openTime = settings.openTime
+  if (settings.openTime === "OPEN_AT") {
+    result.openWeek = settings.openWeek
+    if (settings.openHour !== 99) {
+      result.openHour = settings.openHour
+    }
+  }
+
+  if (!!settings.wakeUp) result.wakeUp = settings.wakeUp
+  if (settings.tagIds.length !== 0) result.tagIds = settings.tagIds
+
+  return result
 }
 
 export default SearchFilter
