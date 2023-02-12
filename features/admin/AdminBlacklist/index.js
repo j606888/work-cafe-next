c363b84682871a389e5a66db05722a8d206e6bcc
James <j606888@gmail.com>
2023-02-12 22:09:14 +0800

    Replace ChainStore with blacklist

--- features/admin/AdminBlacklist/index.js
+++ features/admin/AdminBlacklist/index.js
@@ -1,95 +1,61 @@
 import {
   Box,
-  Button,
   Divider,
   Grid,
-  IconButton,
   Paper,
-  TextField,
-  Typography,
 } from "@mui/material"
-import DeleteIcon from "@mui/icons-material/Delete"
-import { useState, useEffect } from "react"
-import { getBlacklists, createBlacklist, deleteBlacklist } from "api/blacklist"
-import { hideUnqualifiedStores } from "api/stores"
+import useSWR from "swr"
 
 const AdminBlacklist = () => {
-  const [blacklists, setBlacklists] = useState([])
-  const [keyword, setKeyword] = useState("")
-
-  async function fetchBlacklists() {
-    const res = await getBlacklists()
-    setBlacklists(res)
-  }
-
-  useEffect(() => {
-    fetchBlacklists()
-  }, [])
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    if (keyword === "") return
-    setKeyword("")
-
-    await createBlacklist({ keyword })
-    fetchBlacklists()
-  }
-
-  async function handleDelete(id) {
-    await deleteBlacklist({ id })
-    fetchBlacklists()
-  }
-
-  async function handleRefreshStore() {
-    await hideUnqualifiedStores()
-  }
+  const { data: chainStores } = useSWR("/admin/chain-stores")
+  if (!chainStores) return <div>Loading...</div>
 
   return (
     <Paper sx={{ p: 4 }}>
-      <form onSubmit={handleSubmit}>
-        <Typography variant="h5" mb={2}>
-          New Blacklist Keyword
-        </Typography>
-        <Box sx={{ display: "flex", alignItems: "center", gap: 4 }}>
-          <TextField
-            label="keyword..."
-            variant="filled"
-            onChange={(e) => setKeyword(e.target.value)}
-            value={keyword}
-          />
-          <Button variant="contained" type="submit">
-            Create
-          </Button>
-          <Button sx={{ marginLeft: "auto" }} onClick={handleRefreshStore}>
-            Refresh Hiding Store
-          </Button>
-        </Box>
-      </form>
+      <h1>連鎖店清單</h1>
       <Divider sx={{ marginY: 4 }} />
+      <h3>白名單</h3>
       <Grid container spacing={2}>
-        {blacklists.map((blacklist) => {
-          return (
-            <Grid
-              item
-              key={blacklist.id}
-              xs={4}
-              md={3}
-              lg={2}
-              sx={{ display: "flex", alignItems: "center" }}
-            >
-              <IconButton
-                aria-label="delete"
-                onClick={() => handleDelete(blacklist.id)}
+        {chainStores
+          .filter((chainStore) => !chainStore.isBlacklist)
+          .map((chainStore) => {
+            return (
+              <Grid
+                item
+                key={chainStore.id}
+                xs={4}
+                md={3}
+                lg={2}
+                sx={{ display: "flex", alignItems: "center" }}
               >
-                <DeleteIcon />
-              </IconButton>
-              <Box component="span" ml={1}>
-                {blacklist.keyword}
-              </Box>
-            </Grid>
-          )
-        })}
+                <Box component="span" ml={1}>
+                  {chainStore.name}({chainStore.chainStoreMapsCount})
+                </Box>
+              </Grid>
+            )
+          })}
+      </Grid>
+      <Divider sx={{ marginY: 4 }} />
+      <h3>黑名單</h3>
+      <Grid container spacing={2}>
+        {chainStores
+          .filter((chainStore) => chainStore.isBlacklist)
+          .map((chainStore) => {
+            return (
+              <Grid
+                item
+                key={chainStore.id}
+                xs={4}
+                md={3}
+                lg={2}
+                sx={{ display: "flex", alignItems: "center" }}
+              >
+                <Box component="span" ml={1}>
+                  {chainStore.name}({chainStore.chainStoreMapsCount})
+                </Box>
+              </Grid>
+            )
+          })}
       </Grid>
     </Paper>
   )
