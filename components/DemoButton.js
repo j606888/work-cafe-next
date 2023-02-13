716ebfde60152f2c4baedc1d6c67b6710a5a4514
James <j606888@gmail.com>
2023-02-13 23:46:01 +0800

    Add demo button

---
+++ components/DemoButton.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// This is for demo purpose, can be delete after done.
+const DemoButton = () => {
+  return (
+    <button
+      style={{
+        padding: "6px 16px",
+        color: "#FFFFFF",
+        backgroundColor: "#1976d2",
+        borderRadius: "4px",
+        textTransform: "uppercase",
+        cursor: "pointer",
+        border: 0,
+        outline: 0,
+        fontWeight: 500,
+        fontSize: "0.875rem",
+      }}
+    >
+      button 1
+    </button>
+  )
+}
+
+export default DemoButton
