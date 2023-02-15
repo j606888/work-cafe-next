394a5f88c0f9bfc19b01f5387919b36361c826c9
James <j606888@gmail.com>
2023-02-15 21:54:05 +0800

    Try redirect with og

--- pages/share/[placeId].js
+++ pages/share/[placeId].js
@@ -1,17 +1,25 @@
 import { getStoreByPlaceId } from "api/stores"
 
-export default function SharePage({ placeId }) {
+export default function SharePage({  }) {
   return null
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params, res }) {
   const { placeId } = params
-  const res = await getStoreByPlaceId(placeId)
-  const place = res.data
-  const { lat, lng } = place
+  const storeRes = await getStoreByPlaceId(placeId)
+  const { lat, lng, name, address, photos } = storeRes.data
+
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1200, stale-while-revalidate=600"
+  )
+  res.setHeader("Content-Type", "text/html")
+  res.write(
+    `<!DOCTYPE html><html><head><meta property="og:title" content="${name}"><meta property="og:description" content="${address}"><meta property="og:image" content="${photos[0]}"><meta http-equiv="refresh" content="0; url=/map/place/${placeId}/@${lat},${lng},17z"></head><body><p>Redirecting...</p></body></html>`
+  )
+  res.end()
+
   return {
-    redirect: {
-      destination: `/map/place/${placeId}/@${lat},${lng},17z`
-    }
+    props: {}
   }
 }
