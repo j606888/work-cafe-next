1ab1a8f7ed78c043852889b2c8ba8fa79a64e04b
James <j606888@gmail.com>
2023-02-22 23:44:18 +0800

    Not found

--- features/StoreList/NoMatch.js
+++ features/StoreList/NoMatch.js
@@ -1,18 +1,43 @@
+import { grey02 } from 'constants/color'
 import { devices } from 'constants/styled-theme'
 import React from 'react'
 import styled from 'styled-components'
 
+const REPORT_URL = "https://forms.gle/ievHHnQT6U3UNQEdA"
 const NoMatch = () => {
   return (
     <Container>
-      <Img src="/no_match_cat.jpg" alt="no match" />
-      <h3>對不起</h3>
-      <p>找不到任何店家，請試著使用更少的篩選。</p>
+      <Img src="/not_found.png" alt="not_found" />
+      <H3>找不到相符的咖啡店</H3>
+      <P>請嘗試其他搜尋關鍵字，或減少篩選條件。</P>
+      <P>
+        若仍找不到店家，請<A href={REPORT_URL} target="_blank">回報問題</A>，我們將儘速處理。
+      </P>
     </Container>
   )
 }
 
+const H3 = styled.h3`
+  font-size: 24px;
+  color: ${grey02};
+  font-weight: 700;
+`
+
+const P = styled.p`
+  font-size: 16px;
+  color: ${grey02};
+`
+
+const A = styled.a`
+  color: #5c8aff;
+`
+
 const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   text-align: center;
   color: #333;
   background-color: #fff;
