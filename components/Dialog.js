5d92b422f542dff6cc83012f44c5482035c883bf
James <j606888@gmail.com>
2023-03-02 23:15:53 +0800

    Better Dialog

---
+++ components/Dialog.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Dialog as MuiDialog, IconButton } from "@mui/material"
+import CloseButton from '@mui/icons-material/Close'
+import { grey01 } from 'constants/color'
+
+
+const PaperProps = {
+  style: {
+    borderRadius: "20px",
+    padding: "34px 38px 24px",
+    display: "flex",
+    alignItems: "center",
+    minWidth: "300px",
+    position: "relative",
+  },
+}
+
+const IconButtonSx = {
+  position: "absolute",
+  right: "12px",
+  top: "12px",
+  color: grey01,
+  "&:hover": {
+    cursor: "pointer",
+  },
+}
+
+const Dialog = ({ open, onClose, children }) => {
+  return (
+    <MuiDialog open={open} onClose={onClose} PaperProps={PaperProps}>
+      <IconButton onClick={onClose} sx={IconButtonSx}>
+        <CloseButton />
+      </IconButton>
+      {children}
+    </MuiDialog>
+  )
+}
+
+export default Dialog
